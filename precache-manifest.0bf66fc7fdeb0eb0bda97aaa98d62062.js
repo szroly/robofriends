d3d5e45b4efbae79e998c35852454ba331d18b1f
szroly <sep.roland@gmail.com>
2020-02-29 22:17:41 +0100

    Updates

---
+++ precache-manifest.0bf66fc7fdeb0eb0bda97aaa98d62062.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5fbf893ebb8871372acdfd2056b244",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "28db74d851e611b7b374",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2fe371b6ff27b0545266",
+    "url": "/robofriends/static/css/main.5976f339.chunk.css"
+  },
+  {
+    "revision": "28db74d851e611b7b374",
+    "url": "/robofriends/static/js/2.71af9bae.chunk.js"
+  },
+  {
+    "revision": "2fe371b6ff27b0545266",
+    "url": "/robofriends/static/js/main.4996ab29.chunk.js"
+  },
+  {
+    "revision": "1eafa76e58301bf2aef6",
+    "url": "/robofriends/static/js/runtime~main.b45798a7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
